168dfd7d183a37110fbc5298ef047722e6f2fd3a
ChengLeonardo <leonardo.chenget12de1@gmail.com>
2024-09-05 11:25:10 -0300

    fsdfa

--- Actividad8/Biblioteca/obj/Debug/net8.0/Biblioteca.AssemblyInfo.cs
+++ Actividad8/Biblioteca/obj/Debug/net8.0/Biblioteca.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cbd326f5276e56aca67de7000fe8436d00e71e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+316b5c9db0aefe1347148f7960014f4282eff204")]
 [assembly: System.Reflection.AssemblyProductAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
